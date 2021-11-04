65faec9a9bf8aa58cded3b84cccecab924fd1c32
Renato <Renato@DESKTOP-2E73I82>
2021-11-03 22:30:54 -0300

    nova impressao e mudança pra apenas o administrador conseguir cadastrar
    a empresa

--- LeitorDigital/src/main/java/View/DadosEmpresa.java
+++ LeitorDigital/src/main/java/View/DadosEmpresa.java
@@ -155,6 +155,9 @@ public class DadosEmpresa extends JFrame {
 		contentPane.add(scrollPane);
 		
 		JButton btnInserir = new JButton("Inserir");
+		if(a==1 || a==2) {
+			btnInserir.setEnabled(false);
+		}
 		btnInserir.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				AddEmpresa addemp=new AddEmpresa();
